fe9710d215d9504d409f97fd72aa8e4e2384a00d
PrathyushaChirumamilla <prathyusha.chirumamilla@gmail.com>
2019-11-21 07:45:32 +0530

    validations included

--- CSMApplication/CSMA.js
+++ CSMApplication/CSMA.js
@@ -7,15 +7,68 @@ function store()
     var confirm_password= document.getElementById('confirm_password').value;
     var mobile_number= document.getElementById('mobile_number').value;
     var email= document.getElementById('email').value;
-    
-    var registered_users= {
+    console.log(company_name);
+    console.log(mobile_number);
+    console.log(email);
+    if (confirm_password != password)
+    {
+        alert("Password and confirm Password did not match")
+    }
+    else if (company_name=="" || mobile_number=="" || email=="" )
+    {
+        alert("Fields must not be empty");
+    }
+    //if(confirm_password == password)
+    /* else if(localStorage.getItem('users') != null)
+    {
+        var save_data=JSON.parse(localStorage.getItem('users'));
+        for ( var i=0;i<save_data.length;i++)
+        {
+            if(save_data[i].company_name==company_name)
+            {
+                alert("Company with name "+company_name+" already exists. Try again!");
+                break;
+                //empty_values();
+            }
+        }
+
+        ///adding data below
+       /*  var registered_users= {
             company_name:company_name,
             password: password,
-            confirm_passwordc:confirm_password,
             mobile_number:mobile_number,
             email:email,
             time: showTimeAndDate()
       }
+        var users=JSON.parse(localStorage.getItem('users'));
+        users.push(registered_users);
+        localStorage.setItem('users',JSON.stringify(users));
+      empty_values(); */
+ 
+   
+        ///adding data below
+  //  } */
+    else 
+    {
+        /* if(localStorage.getItem('users') != null)
+        var save_data=JSON.parse(localStorage.getItem('users'));
+        for ( var i=0;i<save_data.length;i++)
+        {
+            if(save_data[i].company_name==company_name)
+            {
+                alert("Company with name "+company_name+" already exists. Try again!");
+                //empty_values();
+                break;
+            }
+        } */
+            var registered_users= {
+            company_name:company_name,
+            password: password,
+            mobile_number:mobile_number,
+            email:email,
+            time: showTimeAndDate()
+      }
+      var flag=0;
 
       if(localStorage.getItem('users') == null){
         localStorage.setItem('users', JSON.stringify([]));
@@ -25,19 +78,41 @@ function store()
         console.log(users);
         console.log("inside add comments");
         empty_values();
+        alert("Data saved successfully! Redirecting to login page");
+        location.href="login.html";
     }
     else{
         var users=JSON.parse(localStorage.getItem('users'));
+        //adding code here
+        for ( var i=0;i<users.length;i++)
+        {
+            if(users[i].company_name==company_name)
+            {
+                alert("Company with name "+company_name+" already exists. Try again!");
+                //empty_values();
+                flag=1;
+                break;
+            }
+        } 
+        //adding code here
+        if ( flag!=1 )
+        {
         users.push(registered_users);
         localStorage.setItem('users',JSON.stringify(users));
+        empty_values();
+        alert("Data saved successfully! Redirecting to login page");
+        location.href="login.html";
+        }
       //location.reload();
-      empty_values();
- 
-   
+     // empty_values();
     }
  
+    }
+    /* else
+    { alert("Password and confirm Password did not match")
+      //empty_values();
+    } */
 }
-
 function empty_values()
 {
     document.getElementById('company_name').value='';
@@ -47,7 +122,8 @@ function empty_values()
     document.getElementById('email').value='';
 }
 
-function showTimeAndDate(){
+function showTimeAndDate()
+{
     var date= new Date();
      return date.getHours() + " : " + date.getMinutes() +"\t\t\t" +date.getDate() +"/" + (date.getMonth()+1) +"/"+date.getFullYear()
 }
